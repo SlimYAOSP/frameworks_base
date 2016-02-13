41b11d5ddca7912943edd44d643b8e8d2e89a8fa
GWX <kagwind@gmail.com>
2016-02-13 10:06:03 +0100

    Bring back SmartPullDown to SlimActions.
    patchset 2: rebase
    
    Change-Id: Ia2d71316f09f646ea674fafb2f745ed949494db5

--- core/java/com/android/internal/util/slim/ActionConstants.java
+++ core/java/com/android/internal/util/slim/ActionConstants.java
@@ -60,6 +60,7 @@ public class ActionConstants {
     public static final String ACTION_LOCKDOWN             = "**lockdown**";
     public static final String ACTION_SOUND                = "**sound**";
     public static final String ACTION_USERS                = "**users**";
+    public static final String ACTION_SMART_PULLDOWN       = "**smart_pulldown**";
 
     // no action
     public static final String ACTION_NULL                 = "**null**";
