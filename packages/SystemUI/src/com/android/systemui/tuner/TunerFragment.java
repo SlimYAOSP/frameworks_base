b8673d6dcf7b7f16460b5743a70c4dddaa1bf787
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-02-13 10:35:30 +0100

    [2/2] frameworks_base: Settings: Smart Pulldown
    Change-Id: I86899ef6ee08deffa52e17a56b8545712ad4076f
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -60,6 +60,7 @@ public class TunerFragment extends PreferenceFragment implements OnPreferenceCha
     private static final String STATUS_BAR_BATTERY_STYLE_TEXT = "6";
 
     private static final String QUICK_PULLDOWN = "quick_pulldown";
+    private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
     private static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
     private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
@@ -78,6 +79,7 @@ public class TunerFragment extends PreferenceFragment implements OnPreferenceCha
     private ListPreference mStatusBarBatteryShowPercent;
 
     private ListPreference mQuickPulldown;
+    private ListPreference mSmartPulldown;
 
     private SwitchPreference mShowBrightnessSlider;
 
@@ -147,6 +149,13 @@ public class TunerFragment extends PreferenceFragment implements OnPreferenceCha
         mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
         mQuickPulldown.setOnPreferenceChangeListener(this);
         updatePulldownSummary(quickPulldownValue);
+
+        mSmartPulldown = (ListPreference) findPreference(PREF_SMART_PULLDOWN);
+        int smartPulldownValue = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_SMART_PULLDOWN, 0, UserHandle.USER_CURRENT);
+        mSmartPulldown.setValue(String.valueOf(smartPulldownValue));
+        mSmartPulldown.setOnPreferenceChangeListener(this);
+        updateSmartPulldownSummary(smartPulldownValue);
 
         mShowBrightnessSlider = (SwitchPreference) findPreference(QS_SHOW_BRIGHTNESS_SLIDER);
         int showBrightnessSlider = Settings.Secure.getIntForUser(resolver,
@@ -267,6 +276,12 @@ public class TunerFragment extends PreferenceFragment implements OnPreferenceCha
                     quickPulldownValue, UserHandle.USER_CURRENT);
             updatePulldownSummary(quickPulldownValue);
             return true;
+        } else if (preference == mSmartPulldown) {
+            int smartPulldownValue = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.QS_SMART_PULLDOWN,
+                    smartPulldownValue, UserHandle.USER_CURRENT);
+            updateSmartPulldownSummary(smartPulldownValue);
+            return true;
         } else if (preference == mShowBrightnessSlider) {
             Settings.Secure.putIntForUser(resolver, Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER,
                     mShowBrightnessSlider.isChecked() ? 0 : 1, UserHandle.USER_CURRENT);
@@ -343,6 +358,31 @@ public class TunerFragment extends PreferenceFragment implements OnPreferenceCha
                     ? R.string.quick_pulldown_summary_left
                     : R.string.quick_pulldown_summary_right);
             mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary, direction));
+        }
+    }
+
+    private void updateSmartPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Smart pulldown deactivated
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
+        } else {
+            String type = null;
+            switch (value) {
+                case 1:
+                    type = res.getString(R.string.smart_pulldown_dismissable);
+                    break;
+                case 2:
+                    type = res.getString(R.string.smart_pulldown_persistent);
+                    break;
+                default:
+                    type = res.getString(R.string.smart_pulldown_all);
+                    break;
+            }
+            // Remove title capitalized formatting
+            type = type.toLowerCase();
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
         }
     }
 }
