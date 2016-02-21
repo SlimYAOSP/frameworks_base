31f7a226d96b1fbf1160c730b2bb5f3e61d2b64d
Danesh Mondegarian <daneshm90@gmail.com>
2016-02-21 12:13:32 +0100

    [1/2] frameworks_base: Code: statusbar brightness control
    SystemUI : Port forward statusbar brightness control (Danesh Mondegarian <daneshm90@gmail.com>)
    
    Brings back the status bar brightness control from cm-10.2/cm-11
    
    Change-Id: Icc04e7da3286e9925f8b90528c1e022b33d33e9d
    
    SystemUI: improve brightness slider behavior
    
    Mimic the brightness slider behavior in the statusbar.
    This adds logic to make the statusbar slider also work with automatic
    brightness mode enabled and it will instead adjust the temporary automatic
    brightness overrride.
    
    Change-Id: I78814a785decc83df94844fcc2f489c8cbec2b65
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: status bar brightness: store value as int
    
    Non-automatic brightness value is stored as int, not float.
    
    Symptom: adjust the brightness in the status bar, the brightness
             slider in the notification bar will always be set to full
    
    SystemUI: fix status bar brightness slider eating events
    
    In some cases it would eat the event and not lose focus to the rest of
    the system.
    
    Change-Id: I94d532d09ea762edf7ac037043ed023caf42c401
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI : fix brightness slider for automatic brightness
    
    BRIGHTNESS_ADJ_RESOLUTION was changed to 2048 from 100.
    Update the calculation to reflect this change.
    
    Change-Id: I560109e555e3077c7247856e63b017007098ba1f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3506,6 +3506,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to control brightness from status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
