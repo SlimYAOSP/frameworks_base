ac3f2e282b4b07edfb8187964f72fc13ecd8fc01
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-05-16 12:29:18 +0200

    LED/Keyguard: Only consider battery full when we are done charging
    As it is in kernel side implementation and as it actually makes sense.
    
    Change-Id: I8a0251f6a84619c19103cc7ee96013239f38f350
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -815,21 +815,18 @@ public final class BatteryService extends SystemService {
                     // "Pulse low battery light" is disabled, no lights.
                     mBatteryLight.turnOff();
                 }
-            } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
-                    || status == BatteryManager.BATTERY_STATUS_FULL) {
-                if (status == BatteryManager.BATTERY_STATUS_FULL || level >= 90) {
-                    // Battery is full or charging and nearly full
-                    mBatteryLight.setColor(mBatteryFullARGB);
-                } else {
+            } else if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
                     if (isHvdcpPresent()) {
-                        // Battery is connected to HVDCP charger
+                        // Battery is charging on HVDCP charger
                         mBatteryLight.setFlashing(mBatteryMediumARGB, Light.LIGHT_FLASH_TIMED,
                                 mBatteryLedOn, mBatteryLedOn);
                     } else {
-                        // Battery is charging and halfway full
+                        // Battery is charging
                         mBatteryLight.setColor(mBatteryMediumARGB);
                     }
-                }
+            } else if (status == BatteryManager.BATTERY_STATUS_FULL) {
+                // Battery is full
+                mBatteryLight.setColor(mBatteryFullARGB);
             } else {
                 // No lights if not charging and not low
                 mBatteryLight.turnOff();
